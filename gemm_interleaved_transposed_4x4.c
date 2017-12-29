ada6bb1cfca498d487e5b992d98b011c128caeb3
Shuai Yuan <ysh329@users.noreply.github.com>
2017-12-29 19:20:24 +0800

    Create gemm_interleaved_transposed_4x4.c

---
+++ gemm_interleaved_transposed_4x4.c
@@ -0,0 +1,18 @@
+    for (int p = 0; p < aI_width; p += 8) {
+        CL_ELEM_TYPE aa = vload4(0, aI + row * aI_width + p),
+                     bb = vload4(0, bT + col * aI_width + p);
+
+        c00 += (CL_ELEM_TYPE)aa.s0 * bb;// + (CL_ELEM_TYPE)aa.s0 * bb;
+        c10 += (CL_ELEM_TYPE)aa.s1 * bb;// + (CL_ELEM_TYPE)aa.s1 * bb; 
+        c20 += (CL_ELEM_TYPE)aa.s2 * bb;// + (CL_ELEM_TYPE)aa.s2 * bb;
+        c30 += (CL_ELEM_TYPE)aa.s3 * bb;// + (CL_ELEM_TYPE)aa.s3 * bb;
+
+        aa = vload4(0, aI + row * aI_width + p+4);
+        bb = vload4(0, bT + col * aI_width + p+4);
+
+        c00 += (CL_ELEM_TYPE)aa.s0 * bb;
+        c10 += (CL_ELEM_TYPE)aa.s1 * bb; 
+        c20 += (CL_ELEM_TYPE)aa.s2 * bb;
+        c30 += (CL_ELEM_TYPE)aa.s3 * bb;
+
+    }
